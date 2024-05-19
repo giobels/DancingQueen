c5624a35507bebf9187a3352c0ba1d084fd31e08
giobels <giovanna.beltrao@sptech.school>
2024-05-18 22:53:59 -0300

    conexão com web-data-viz cadastro

--- src/database/DancingQueen-script.sql
+++ src/database/DancingQueen-script.sql
@@ -1,6 +1,6 @@
 create database DancingQueen;
 use DancingQueen;
-
+-- drop database sensfit
 create table Usuario(
 idUsuario int auto_increment,
 nome varchar(20) not null,
@@ -9,3 +9,5 @@ senha varchar(6) not null,
 
 constraint pkUsuario primary key (idUsuario),
 constraint uniEmail unique key (email));
+
+select*from usuario;
